117dc1ffa669b12cabe25395a693ad50ee6dffe8
sadmin <abdulhassan33@gmail.com>
2026-01-30 22:07:35 +0530

    Added User class skeleton

---
+++ src/main/java/com/abdul/ecommerce/model/User.java
@@ -0,0 +1,16 @@
+package com.abdul.ecommerce.model;
+
+public class User {
+	
+	private int id, phone;
+	private String name, email, password, role, createdAt, updatedAt;
+	
+	private void changePassword() {
+		
+	}
+	
+	private void updateProfile() {
+		
+	}
+
+}
