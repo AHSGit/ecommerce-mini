ef36589ed38f9324747905494ee74547d5d1d10a
sadmin <abdulhassan33@gmail.com>
2026-01-30 22:45:57 +0530

    Implemented encapsulation and constructors in User class

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-1.a: Added User class skeleton
+## 1.a: Added User class skeleton
+## 1.b: Implemented encapsulation and constructors in User class
--- src/main/java/com/abdul/ecommerce/model/User.java
+++ src/main/java/com/abdul/ecommerce/model/User.java
@@ -1,16 +1,91 @@
 package com.abdul.ecommerce.model;
 
+import java.time.LocalDateTime;
+
 public class User {
 	
-	private int id, phone;
-	private String name, email, password, role, createdAt, updatedAt;
+	private int id;
+	private String phone, name, email, password, role;
+	private LocalDateTime createdAt, updatedAt;
 	
-	private void changePassword() {
-		
+	public void changePassword() {}
+	
+	public void updateProfile() {}
+	
+	public User() {}
+	
+	public User(String name, String phone, String email, String password, String role, LocalDateTime createdAt, LocalDateTime updatedAt) {
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
 	}
-	
-	private void updateProfile() {
-		
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
 	}
 
 }
